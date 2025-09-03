f67f7077a01c536ce703e39f076be90c242db896
Abdessamad Zakari <abde.zakari@gmail.com>
2025-09-03 11:39:45 +0200

    initial commit

---
+++ src/resources/js/Pages/Testa.jsx
@@ -0,0 +1,14 @@
+// import Layout from '@/layouts/authenticated';
+import { Head } from '@inertiajs/react';
+
+export default function Testa({ user }) {
+    return (
+        // <Layout>
+        <div>
+            <Head title="Welcome" />
+            <h1>Welcome</h1>
+            <p className="text-red-300">Hello {user}, welcome to Inertia.</p>
+        </div>
+        // </Layout>
+    )
+}
